19049278a5f6b7f6ee4a016974260ff7e6f441ca
Adler Quispe Montañez <adler.233@hotmail.com>
2023-04-04 09:17:22 -0500

    Agregar archivos de proyecto.

---
+++ granjero/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿
+namespace granjero
+{
+    partial class ejercicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ejercicio));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lsBote = new System.Windows.Forms.PictureBox();
+            this.btnOrigen = new System.Windows.Forms.Button();
+            this.btnSalida = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.btnTrasladar = new System.Windows.Forms.Button();
+            this.lsOrigen = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lssBote = new System.Windows.Forms.ListBox();
+            this.lsLLegada = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tHora = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lsBote)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList2.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // lsBote
+            // 
+            this.lsBote.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lsBote.Image = ((System.Drawing.Image)(resources.GetObject("lsBote.Image")));
+            this.lsBote.Location = new System.Drawing.Point(420, 360);
+            this.lsBote.Name = "lsBote";
+            this.lsBote.Size = new System.Drawing.Size(214, 117);
+            this.lsBote.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lsBote.TabIndex = 4;
+            this.lsBote.TabStop = false;
+            this.lsBote.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // btnOrigen
+            // 
+            this.btnOrigen.Location = new System.Drawing.Point(180, 281);
+            this.btnOrigen.Name = "btnOrigen";
+            this.btnOrigen.Size = new System.Drawing.Size(75, 23);
+            this.btnOrigen.TabIndex = 5;
+            this.btnOrigen.Text = "Subir al Bote";
+            this.btnOrigen.UseVisualStyleBackColor = true;
+            this.btnOrigen.Click += new System.EventHandler(this.btnOrigen_Click);
+            // 
+            // btnSalida
+            // 
+            this.btnSalida.Location = new System.Drawing.Point(517, 196);
+            this.btnSalida.Name = "btnSalida";
+            this.btnSalida.Size = new System.Drawing.Size(75, 23);
+            this.btnSalida.TabIndex = 6;
+            this.btnSalida.Text = "Subir al Bote";
+            this.btnSalida.UseVisualStyleBackColor = true;
+            this.btnSalida.Click += new System.EventHandler(this.btnSalida_Click);
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Location = new System.Drawing.Point(315, 399);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegresar.TabIndex = 7;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
+            // 
+            // btnTrasladar
+            // 
+            this.btnTrasladar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnTrasladar.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTrasladar.Location = new System.Drawing.Point(656, 399);
+            this.btnTrasladar.Name = "btnTrasladar";
+            this.btnTrasladar.Size = new System.Drawing.Size(77, 23);
+            this.btnTrasladar.TabIndex = 8;
+            this.btnTrasladar.Text = "Trasladar";
+            this.btnTrasladar.UseVisualStyleBackColor = false;
+            this.btnTrasladar.Click += new System.EventHandler(this.btnTrasladar_Click);
+            // 
+            // lsOrigen
+            // 
+            this.lsOrigen.BackColor = System.Drawing.Color.Green;
+            this.lsOrigen.ForeColor = System.Drawing.Color.White;
+            this.lsOrigen.FormattingEnabled = true;
+            this.lsOrigen.Items.AddRange(new object[] {
+            "Cesta de heno de alfalfa",
+            "Conejo",
+            "Aguila Real",
+            "Granjero"});
+            this.lsOrigen.Location = new System.Drawing.Point(27, 258);
+            this.lsOrigen.Name = "lsOrigen";
+            this.lsOrigen.Size = new System.Drawing.Size(130, 69);
+            this.lsOrigen.TabIndex = 10;
+            this.lsOrigen.SelectedIndexChanged += new System.EventHandler(this.lsOrigen_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(217, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 52);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lssBote
+            // 
+            this.lssBote.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lssBote.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lssBote.FormattingEnabled = true;
+            this.lssBote.Location = new System.Drawing.Point(451, 367);
+            this.lssBote.Name = "lssBote";
+            this.lssBote.Size = new System.Drawing.Size(141, 30);
+            this.lssBote.TabIndex = 12;
+            // 
+            // lsLLegada
+            // 
+            this.lsLLegada.BackColor = System.Drawing.Color.Green;
+            this.lsLLegada.ForeColor = System.Drawing.SystemColors.Window;
+            this.lsLLegada.FormattingEnabled = true;
+            this.lsLLegada.Location = new System.Drawing.Point(612, 163);
+            this.lsLLegada.Name = "lsLLegada";
+            this.lsLLegada.Size = new System.Drawing.Size(133, 82);
+            this.lsLLegada.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Font = new System.Drawing.Font("SimSun", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(320, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 24);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "EL GRANJERO";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(300, 47);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(80, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(499, 47);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(76, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 16;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(404, 47);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(69, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 17;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 14);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "HORA";
+            // 
+            // tHora
+            // 
+            this.tHora.AutoSize = true;
+            this.tHora.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tHora.Location = new System.Drawing.Point(45, 33);
+            this.tHora.Name = "tHora";
+            this.tHora.Size = new System.Drawing.Size(17, 26);
+            this.tHora.TabIndex = 19;
+            this.tHora.Text = ".";
+            this.tHora.Click += new System.EventHandler(this.tHora_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.SystemColors.Info;
+            this.btnSalir.Location = new System.Drawing.Point(713, 14);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 20;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // ejercicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.tHora);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lsLLegada);
+            this.Controls.Add(this.lssBote);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lsOrigen);
+            this.Controls.Add(this.btnTrasladar);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnSalida);
+            this.Controls.Add(this.btnOrigen);
+            this.Controls.Add(this.lsBote);
+            this.Controls.Add(this.pictureBox3);
+            this.Name = "ejercicio";
+            this.Text = "Ejercicio del Granjero";
+            this.Load += new System.EventHandler(this.ejercicio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lsBote)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox lsBote;
+        private System.Windows.Forms.Button btnOrigen;
+        private System.Windows.Forms.Button btnSalida;
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.Button btnTrasladar;
+        private System.Windows.Forms.ListBox lsOrigen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox lssBote;
+        private System.Windows.Forms.ListBox lsLLegada;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label tHora;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ granjero/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace granjero
+{
+    public partial class ejercicio : Form
+    {
+        public ejercicio()
+        {
+            InitializeComponent();
+            //imagen
+            /*
+            Bitmap img = new Bitmap(Application.StartupPath + @"\imagen\fondo1.png");
+            this.BackgroundImage = img;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            */
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lsOrigen1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void perdisteOrigen()
+        {
+            if((lsOrigen.Items.Count == 3 && lsOrigen.Items[1]=="Conejo" && lsOrigen.Items[0]== "Cesta de heno de alfalfa" &&
+                lsOrigen.Items[2]== "Aguila Real"))
+            {
+                MessageBox.Show("Error el Granjero va 2do");
+            }
+            if ((lsOrigen.Items.Count == 2 && lsOrigen.Items[1] == "Conejo" && lsOrigen.Items[0] == "Aguila Real") ||
+               (lsOrigen.Items.Count == 2 && lsOrigen.Items[0] == "Conejo" && lsOrigen.Items[1] == "Aguila Real"))
+            {
+                MessageBox.Show("Error La Aguila se Comera al Conejo");
+
+            }
+
+            if (lsOrigen.Items.Count==3 && lsOrigen.Items[0]=="Conejo" && 
+                lsOrigen.Items[1]== "Cesta de heno de alfalfa" && lsOrigen.Items[2]=="Aguila Real")
+            {
+                MessageBox.Show("Error1");
+            }
+           
+            if ((lsOrigen.Items.Count == 2 && lsOrigen.Items[1] == "Conejo" && lsOrigen.Items[0] == "Cesta de heno de alfalfa") ||
+                (lsOrigen.Items.Count == 2 && lsOrigen.Items[0] == "Conejo" && lsOrigen.Items[1] == "Cesta de heno de alfalfa"))
+            {
+                MessageBox.Show("Error el Conejo se comera la Alfafa");
+
+            }
+          
+          
+
+        }
+        public void perdisteBote()
+        {
+            if ((lssBote.Items.Count == 2 && lssBote.Items[0] == "Conejo" && lssBote.Items[1] == "Cesta de heno de alfalfa") ||
+                (lssBote.Items.Count == 2 && lssBote.Items[1] == "Conejo" && lssBote.Items[0] == "Cesta de heno de alfalfa")||
+                (lssBote.Items.Count == 2 && lssBote.Items[0] == "Conejo" && lssBote.Items[1] == "Aguila Real") ||
+                 (lssBote.Items.Count == 2 && lssBote.Items[1] == "Conejo" && lssBote.Items[0] == "Aguila Real") ||
+                  (lssBote.Items.Count == 2 && lssBote.Items[0] == "Aguila Real" && lssBote.Items[1] == "Cesta de heno de alfalfa") ||
+                (lssBote.Items.Count == 2 && lssBote.Items[1] == "Aguila Real" && lssBote.Items[0] == "Cesta de heno de alfalfa"))   
+
+              
+            {
+
+                MessageBox.Show("----------Error no puede conducir el bote sin el Granjero--------- ");
+            }
+
+
+
+   
+     
+
+            if (lssBote.Items.Count == 3 || lssBote.Items.Count == 4)
+            {
+
+                MessageBox.Show("Error Solo Hay espacio para 2");
+            }
+        }
+        public void perdistellegada()
+        {
+            if ((lsLLegada.Items.Count == 2 && lsLLegada.Items[1] == "Conejo" && lsLLegada.Items[0] == "Cesta de heno de alfalfa") ||
+                (lsLLegada.Items.Count == 2 && lsLLegada.Items[0] == "Conejo" && lsLLegada.Items[0] == "Cesta de heno de alfalfa"))
+            {
+                MessageBox.Show("Error5");
+            }
+            if ((lsLLegada.Items.Count == 2 && lsLLegada.Items[1] == "Conejo" && lsLLegada.Items[0] == "Aguila Real") ||
+                (lsLLegada.Items.Count == 2 && lsLLegada.Items[0] == "Conejo" && lsLLegada.Items[0] == "Aguila Real"))
+            {
+                MessageBox.Show("Error6");
+            }
+
+
+        }
+        private void btnOrigen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lssBote.Items.Add(lsOrigen.SelectedItem.ToString());
+                lsOrigen.Items.Remove(lsOrigen.SelectedItem.ToString());
+                perdisteOrigen();
+                perdisteBote();
+                perdistellegada();
+
+
+            }
+
+            catch
+            { }
+        }
+
+        private void btnTrasladar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if(lssBote.Items[0].ToString()=="Granjero"||lssBote.Items[1].ToString()=="Granjero")
+                {
+                    for(int i = 0; i < lssBote.Items.Count; i++)
+                    {
+                        lsLLegada.Items.Add(lssBote.Items[i].ToString());
+
+                    }
+                    lssBote.Items.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("No hay nadie que maneje el Bote");
+                }
+                if (lsLLegada.Items.Count == 4 )
+                {
+                    MessageBox.Show("Felecitaciones ah Ganado");
+
+                }
+
+                perdisteBote();
+            }
+            catch
+            {
+
+            }
+
+
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lsOrigen.Items.Add(lssBote.SelectedItem.ToString());
+                lssBote.Items.Remove(lssBote.SelectedItem.ToString());
+                perdisteOrigen();
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void btnSalida_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lssBote.Items.Add(lsLLegada.SelectedItem.ToString());
+                lsLLegada.Items.Remove(lsLLegada.SelectedItem.ToString());
+                perdistellegada();
+                perdisteBote();
+               
+            }
+
+            
+            catch
+            {
+
+            }
+        }
+
+        private void ejercicio_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            tHora.Text = DateTime.Now.ToString("hh:mm:ss");
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tHora_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lsOrigen_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
